f51886f8a88884a4d19edf75411b0de85d2182f9
oluwatayo Hassan <tayohas@gmail.com>
2023-01-28 17:52:05 +0000

    pushed code for current day

--- js/file.js
+++ js/file.js
@@ -1,1 +1,16 @@
 alert("Wow")
+var currentDay = $('#currentDay');
+
+var colorcode = $('.colorstyle');
+
+console .log(colorcode)
+
+function displayDate(){
+var current = moment().format('dddd, MMMM Do');
+currentDay.text(current);
+console.log(current)
+
+}
+
+displayDate();
+
