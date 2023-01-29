5c9d37f627d348db8302221803b22b31947a0db3
oluwatayo Hassan <tayohas@gmail.com>
2023-01-29 15:44:29 +0000

    updated a reformated timeblock for the HTML code

--- js/file.js
+++ js/file.js
@@ -1,16 +1,1 @@
 alert("Wow")
-var currentDay = $('#currentDay');
-
-var colorcode = $('.colorstyle');
-
-console .log(colorcode)
-
-function displayDate(){
-var current = moment().format('dddd, MMMM Do');
-currentDay.text(current);
-console.log(current)
-
-}
-
-displayDate();
-
